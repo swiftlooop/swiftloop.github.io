2bb0201d61a17340cc35cfae9547dcd5e9612c32
swiftloop <113066032+swiftlooop@users.noreply.github.com>
2023-06-06 18:41:00 +0200

    Delete js directory

--- js/chunks-main-app-72dd08d1d4e4deae.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[744],{4517:function(e,n,t){Promise.resolve().then(t.t.bind(t,3395,23)),Promise.resolve().then(t.t.bind(t,7449,23)),Promise.resolve().then(t.t.bind(t,485,23)),Promise.resolve().then(t.t.bind(t,8668,23))}},function(e){var n=function(n){return e(e.s=n)};e.O(0,[679],function(){return n(1070),n(4517)}),_N_E=e.O()}]);
